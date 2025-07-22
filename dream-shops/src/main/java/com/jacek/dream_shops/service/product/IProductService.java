6f8bea996495673e68f7b2084e70418f2c3650ec
Jacek <laljacek@gmail.com>
2025-07-22 22:01:38 +0200

    created data models for database

---
+++ dream-shops/src/main/java/com/jacek/dream_shops/service/product/IProductService.java
@@ -0,0 +1,8 @@
+package com.jacek.dream_shops.service.product;
+
+import com.jacek.dream_shops.model.Product;
+
+public interface IProductService {
+    Product addProduct(Product product);
+
+}
---
+++ dream-shops/src/main/java/com/jacek/dream_shops/service/product/ProductService.java
@@ -0,0 +1,4 @@
+package com.jacek.dream_shops.service.product;
+
+public class ProductService {
+}
