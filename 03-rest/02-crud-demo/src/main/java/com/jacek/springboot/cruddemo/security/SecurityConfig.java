351f2e25d5c38843c815c20b4bedeb3ee02d81a9
Jacek <laljacek@gmail.com>
2025-07-27 23:18:04 +0200

    added user role based authorization

--- 03-rest/02-crud-demo/src/main/java/com/jacek/springboot/cruddemo/security/SecurityConfig.java
+++ 03-rest/02-crud-demo/src/main/java/com/jacek/springboot/cruddemo/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.jacek.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,4 +35,26 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable when:
+        // 1. Serving non-browser users
+        // 2. Having other authentication methods
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 }
